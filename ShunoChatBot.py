26f54d1610f54d49de785f88887fad2d578e8970
ShunoJunsu <shuno.india@gmail.com>
2026-01-31 11:01:35 +0900

    set page config

--- ShunoChatBot.py
+++ ShunoChatBot.py
@@ -2,3 +2,9 @@ import os
 import streamlit as st
 
 os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_AI_API_KEY"]
+
+st.set_page_config(
+    page_title = "IM4U 코딩 비서",
+    page_icon = ":computer:",
+    layout = "wide"
+)
